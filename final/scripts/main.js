9a22d3767ece13ee9c01afb9717585c5c67a1286
Celina Stillman <celinastillman@Celinas-MacBook-Air.local>
2018-04-23 14:19:36 -0400

    loader real

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -42,6 +42,17 @@ const app = new Vue({
   }
 })
 
+////////////LOADER JS///////////////
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
 
 
 
