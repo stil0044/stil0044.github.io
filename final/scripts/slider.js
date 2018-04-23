50395f4a4dae68a8d4558ddf3ac90e763f9edbfc
Celina Stillman <celinastillman@Celinas-MacBook-Air.local>
2018-04-23 16:35:17 -0400

    Slider

--- final/scripts/slider.js
+++ final/scripts/slider.js
@@ -1,62 +1,58 @@
 // The js for slider goes in this file
 
 
-//Range slider****NO PURPOSE YET
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
 
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
 ////////////////////////////////////////////////////////////////////////////////////
 
 
 ///AUTOMATIC SLIDESHOW//////NOT WORKING??????? IS IT LINKED??
-var slideIndex = 0;
-carousel();
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none"; 
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.display = "block"; 
-    setTimeout(carousel, 2000); // Change image every 2 seconds
-}
 
 
 
 //SLIDESHOW---------https://www.w3schools.com/howto/howto_js_slideshow.asp///
-var slideIndex = 1;
-showSlides(slideIndex);
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+$(document).ready(function(){
+  //call the function when ready
+  slideShow();
 
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("fade");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
+
+//Actually define the slideShow()
+function slideShow(){
+  
+  //*** Conditional & Variables ***//
+  
+    //Define the current img
+    var current = $('#slider .show');
+    //If index != 0/false then show next img
+  var next = current.next().length ? 
+      current.next() :
+      // if index == false then show first img
+      current.siblings().first();
+  
+   //*** Swap out the imgs and class ***//
+   current.hide().removeClass('show');
+   next.fadeIn("slow").addClass('show');
+  
+  
+  //*** Repeat function every 3 seconds ***//
+  setTimeout(slideShow, 3000);
+  
+};
+  
+}); //end ready
+
+
+
+
+
+
+/////FADESLIDE SHOW///////
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
