e38c30e1530458c7e8bbdeef0524ec8d98b34698
Celina Stillman <celinastillman@Celinas-MacBook-Air.local>
2018-04-24 13:16:06 -0400

    css

--- final/scripts/validation.js
+++ final/scripts/validation.js
@@ -4,7 +4,6 @@
 function formValidation()
 {
 var uid = document.registration.userid;
-var passid = document.registration.passid;
 var uname = document.registration.username;
 var uadd = document.registration.address;
 var ucountry = document.registration.country;
@@ -44,17 +43,6 @@ if (uid_len == 0 || uid_len >= my || uid_len < mx)
 {
 alert("User Id should not be empty / length be between "+mx+" to "+my);
 uid.focus();
-return false;
-}
-return true;
-}
-function passid_validation(passid,mx,my)
-{
-var passid_len = passid.value.length;
-if (passid_len == 0 ||passid_len >= my || passid_len < mx)
-{
-alert("Password should not be empty / length be between "+mx+" to "+my);
-passid.focus();
 return false;
 }
 return true;
