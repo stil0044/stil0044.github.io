18727ca3b99a065d2a63158433c4bcf2fe228669
Celina Stillman <celinastillman@Celinas-MacBook-Air.local>
2018-04-24 13:29:30 -0400

    remove sip

--- final/scripts/validation.js
+++ final/scripts/validation.js
@@ -88,19 +88,6 @@ else
 return true;
 }
 }
-function allnumeric(uzip)
-{ 
-var numbers = /^[0-9]+$/;
-if(uzip.value.match(numbers))
-{
-return true;
-}
-else
-{
-alert('ZIP code must have numeric characters only');
-uzip.focus();
-return false;
-}
 }
 function ValidateEmail(uemail)
 {
